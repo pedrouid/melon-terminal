50c9e3006858b83a6bcc188e88da6cd4ba8cbfb2
Ivan Herger <ivan.herger@gmail.com>
2020-01-24 15:02:46 +0100

    Iteration of useCurrency context

--- src/components/Contexts/Currency/Currency.tsx
+++ src/components/Contexts/Currency/Currency.tsx
@@ -1,8 +1,82 @@
-import React, { useState, createContext } from 'react';
+import React, { useState, createContext, useEffect, useMemo } from 'react';
 
-export const CurrencyContext = createContext([{}, () => {}]);
+export const currencyList = ['ETH', 'USD', 'EUR', 'CHF'];
+
+export enum CurrencyStatus {
+  LOADING,
+  SUCCESS,
+  ERROR,
+}
+
+export interface Currency {
+  symbol: string;
+  rate?: number;
+  time?: Date;
+}
+
+export interface CurrencyContext {
+  status: CurrencyStatus;
+  selected: Currency;
+  list: Currency[];
+  switch: (currency: string) => void;
+}
+
+export interface UseCoinAPIProps {
+  base?: string;
+  quote?: string;
+}
+
+export interface CoinAPIResult {
+  time: Date;
+  asset_id_base: string;
+  asset_id_quote: string;
+  rate: number;
+}
+
+export const Currency = createContext<CurrencyContext>({} as CurrencyContext);
 
 export const CurrencyProvider: React.FC = props => {
-  const [state, setState] = useState({});
-  return <CurrencyContext.Provider value={[state, setState]}>{props.children}</CurrencyContext.Provider>;
+  const [currency, setCurrency] = useState<string>('');
+  const [coinApi, setcoinApi] = useState({ state: 'idle', data: {} as CoinAPIResult });
+
+  useEffect(() => {
+    setcoinApi({ state: 'LOADING', data: {} as CoinAPIResult });
+    (async () => {
+      try {
+        const result = await fetch(`https://coinapi.melon.network/?base=ETH&quote=${currency || 'USD'}`);
+        const json = (await result.json()) as CoinAPIResult;
+        setcoinApi({ state: 'SUCCESS', data: json });
+      } catch (e) {
+        setcoinApi({ state: 'ERROR', data: {} as CoinAPIResult });
+      }
+    })();
+  }, [currency]);
+
+  const status = useMemo(() => {
+    if (coinApi.state === 'SUCCESS') {
+      return CurrencyStatus.SUCCESS;
+    }
+
+    if (coinApi.state === 'LOADING') {
+      return CurrencyStatus.LOADING;
+    }
+
+    return CurrencyStatus.ERROR;
+  }, [coinApi.state]);
+
+  const selected = useMemo(() => {
+    return { symbol: currency, rate: coinApi.data.rate, time: coinApi.data.time };
+  }, [currency, coinApi]);
+
+  const list = currencyList.map(token => {
+    return { symbol: token };
+  }) as Currency[];
+
+  const context: CurrencyContext = {
+    status,
+    selected,
+    list,
+    switch: currency => setCurrency(currency),
+  };
+  return <Currency.Provider value={context}>{props.children}</Currency.Provider>;
 };
--- src/hooks/useCurrency.ts
+++ src/hooks/useCurrency.ts
@@ -1,6 +1,6 @@
 import { useContext } from 'react';
-import { CurrencyContext } from '~/components/Contexts/Currency/Currency';
+import { Currency } from '~/components/Contexts/Currency/Currency';
 
 export function useCurrency() {
-  return useContext(CurrencyContext);
+  return useContext(Currency);
 }
