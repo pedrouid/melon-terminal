125b0948ceef518c02d5ed914b868e0ac4c5f26c
ishak-lm <leghlamishak@gmail.com>
2020-02-05 10:52:04 +0100

    [WIP] no value displayed in select

--- src/storybook/components/DropdownIcons/DropdownIcons.tsx
+++ src/storybook/components/DropdownIcons/DropdownIcons.tsx
@@ -8,12 +8,12 @@ export interface DropdownProps extends ReactSelectProps {
   label?: string;
 }
 
-export const DropdownIcons: React.FC<DropdownProps> = ({ name, label, ...rest }) => {
+export const DropdownIcons: React.FC<DropdownProps> = ({ options, name, label, ...rest }) => {
   const form = useFormContext();
-  const { control } = useForm();
-  const connected = !!(form && name);
-  const errors = connected ? form.errors : undefined;
-  const error = !!(errors && errors[name!]);
+  const { control } = form;
+  // const connected = !!(form && name);
+  // const errors = connected ? form.errors : undefined;
+  // const error = !!(errors && errors[name!]);
 
   const Option = (props: any) => {
     return (
@@ -26,21 +26,23 @@ export const DropdownIcons: React.FC<DropdownProps> = ({ name, label, ...rest })
     );
   };
 
+  console.log(form.watch(name));
+  const value = form.watch(name);
+
+  const selected = options?.find(item => item.value === value);
+
   return (
     <D.DropdownWrapper>
       {label && <D.DropdownLabel>{label}</D.DropdownLabel>}
 
       <Controller
-        as={<Select {...rest} components={{ Option }} />}
-        name="reactSelect"
+        as={<Select {...rest} components={{ Option }} options={options} value={selected} />}
+        name={name!}
         control={control}
-        onChange={([selected]) => {
-          // React Select return object instead of value for selecCotion
-          return { value: selected };
-        }}
+        onChange={([selected]) => selected}
       />
 
-      {error && <ErrorMessage errors={form.errors} name={name!} as={<D.DropdownError />} />}
+      {/* {error && <ErrorMessage errors={form.errors} name={name!} as={<D.DropdownError />} />} */}
     </D.DropdownWrapper>
   );
 };
